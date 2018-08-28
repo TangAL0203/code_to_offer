3bf51aef4c766980c12224c2e090212f044a06e8
TangShengqin <15527733782@163.com>
2018-08-28 16:23:34 +0800

    Update OrdinaryInsertionSort.py

--- python/SortingAlgorithm/InsertionSort/OrdinaryInsertionSort.py
+++ python/SortingAlgorithm/InsertionSort/OrdinaryInsertionSort.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 def insert_sort(lists):
     # 插入排序
+    # 将列表分为[有序+无序部分]，每次插入的时候，从无序部分中取第一个元素插入到合适的位置
+    # 长度为n的列表，需要进行n-1次插入，每次插入的位置不是固定的
     count = len(lists)
     for i in range(1, count):
         key = lists[i]
