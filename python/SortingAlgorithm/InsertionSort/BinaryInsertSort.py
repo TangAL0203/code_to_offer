9516345be993101eb0aec82cf0a9ff090474bc2d
TangShengqin <15527733782@163.com>
2018-08-28 17:58:09 +0800

    Update BinaryInsertSort.py

--- python/SortingAlgorithm/InsertionSort/BinaryInsertSort.py
+++ python/SortingAlgorithm/InsertionSort/BinaryInsertSort.py
@@ -1,20 +1,22 @@
 # -*- coding:utf-8 -*-
+# 折半插入排序
 def BinaryInsertSort(array):
     if len(array)<=1:
         return array
     count = len(array)
     for i in range(1,count):
         low = 0
-        high = i-1
+        high = i-1 # low和high表示已经排好序的区间
         value = array[i]
-        # find correct inserted position
+        # 将待插入的元素和之前拍好序的元素做比较，二分缩小区间，找到合适的插入位置，插入位置是low
         while low<=high:
             mid = (low+high)/2
             if value>array[mid]:
                 low = mid+1
             else:
                 high = mid-1
-        # move
+        # 
+        # 先移动后面的，再移动前面的
         for j in range(i-1,low-1,-1):
             array[j+1] = array[j]
         array[low] = value
