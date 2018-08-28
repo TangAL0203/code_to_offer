1231ad72c13a62ca43b706a5bea3e32e790d9034
TangShengqin <15527733782@163.com>
2018-08-28 16:05:39 +0800

    Update quickSort.py

--- python/SortingAlgorithm/ExchangeSort/quickSort.py
+++ python/SortingAlgorithm/ExchangeSort/quickSort.py
@@ -7,6 +7,7 @@ def quick_sort(lists, left, right):
     key = lists[left]
     low = left
     high = right
+    # 一趟快排过程
     while left < right:
         while left < right and lists[right] >= key:
             right -= 1
@@ -14,7 +15,7 @@ def quick_sort(lists, left, right):
         while left < right and lists[left] <= key:
             left += 1
         lists[right] = lists[left]
-    lists[right] = key # 将key值归位
+    lists[right] = key # 将key值归位，此时left==right
     quick_sort(lists, low, left - 1)
     quick_sort(lists, left + 1, high)
     return lists
